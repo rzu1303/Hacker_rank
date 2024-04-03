a3c3be6c125908f817f18249b7cc66979cb3c6b9
rzu1303 <31926280+rzu1303@users.noreply.github.com>
2024-04-04 00:45:01 +0600

    diagonal_difference.py, flipping_bits.py  solved

---
+++ diagonal_difference.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    diagonal1 = 0
+    diagonal2 = 0
+    for i, j in zip(range(n),range(n-1, -1, -1)):
+        diagonal1 = diagonal1+ arr[i][i]
+        diagonal2 = diagonal2+ arr[i][j]
+
+    return abs(diagonal1-diagonal2)
+
+
+if __name__ == '__main__':
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    # fptr.write(str(result) + '\n')
+
+    # fptr.close()
